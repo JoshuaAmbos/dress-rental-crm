96d517ba88ad2abec53b867096ee90ce4e0b43cc
Your GitHub Username <your-github-email@example.com>
2026-09-17 20:59:04 +0800

    Reapply "winforms: added customer table"
    This reverts commit efdab1bb6766a062cf4c632bf334e3cd73713777.

--- CRM.winforms/Forms/MainForm.cs
+++ CRM.winforms/Forms/MainForm.cs
@@ -1,43 +1,74 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Text;
 using System.Windows.Forms;
+using CRM.winforms.Models;
+using CRM.winforms.Views;
 
-namespace CRM.winforms.Forms
+namespace CRM.winforms.Forms;
+
+public partial class MainForm : Form
 {
-    public partial class MainForm : Form
+    private readonly LoginResult _user;
+    private UserControl? _activeView;
+    private CustomerProfilesView? _customerProfilesView;
+
+    // colors
+    public static readonly Color ColorSidebar = Color.FromArgb(52, 30, 33);
+    public static readonly Color ColorSidebarActive = Color.FromArgb(190, 110, 120);
+
+    public MainForm() : this(new LoginResult { Username = "DesignerStaff", Roles = new[] { "admin" } })
     {
-        public MainForm(Models.LoginResult authenticatedUser)
+    }
+
+    public MainForm(LoginResult authenticatedUser)
+    {
+        _user = authenticatedUser ?? throw new ArgumentNullException(nameof(authenticatedUser));
+        InitializeComponent();
+    }
+
+    private void MainForm_Load(object sender, EventArgs e)
+    {
+        if (LicenseManager.UsageMode != LicenseUsageMode.Designtime)
         {
-            InitializeComponent();
-        }
-
-        private void panel1_Paint(object sender, PaintEventArgs e)
-        {
-
-        }
-
-        private void logo_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void MainForm_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-
+            ShowCustomerProfiles();
         }
     }
+
+    private void SwitchView(UserControl view)
+    {
+        if (_activeView == view || panelContents == null) return;
+
+        panelContents.SuspendLayout();
+        panelContents.Controls.Clear();
+        view.Dock = DockStyle.Fill;
+        panelContents.Controls.Add(view);
+        view.BringToFront();
+        panelContents.ResumeLayout();
+
+        _activeView = view;
+    }
+
+    public void ShowCustomerProfiles()
+    {
+        _customerProfilesView ??= new CustomerProfilesView();
+        SwitchView(_customerProfilesView);
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+        ShowCustomerProfiles();
+    }
+
+    private void button2_Click(object sender, EventArgs e)
+    {
+    }
+
+    private void buttonDashboard_Click(object sender, EventArgs e)
+    {
+    }
+
+    private void panel1_Paint(object sender, PaintEventArgs e) { }
+    private void logo_Click(object sender, EventArgs e) { }
+    private void panelContents_Paint(object sender, PaintEventArgs e) { }
 }
