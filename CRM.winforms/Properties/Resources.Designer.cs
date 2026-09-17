efdab1bb6766a062cf4c632bf334e3cd73713777
Your GitHub Username <your-github-email@example.com>
2026-09-17 20:55:27 +0800

    Revert "winforms: added customer table"
    This reverts commit f16f641ba6fc5d4d7cdd9b1fc06e013e27ebd437.

--- CRM.winforms/Properties/Resources.Designer.cs
+++ CRM.winforms/Properties/Resources.Designer.cs
@@ -69,25 +69,5 @@ namespace CRM.winforms.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap search {
-            get {
-                object obj = ResourceManager.GetObject("search", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap search1 {
-            get {
-                object obj = ResourceManager.GetObject("search1", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
     }
 }
