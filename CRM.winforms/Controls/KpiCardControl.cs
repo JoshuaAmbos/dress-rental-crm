edcc66fcc1af3db940fafb68fbdd4b0b5e3f9c11
JoshuaAmbos <j.ambos.554298@umindanao.edu.ph>
2026-09-17 21:02:19 +0800

    Reapply "Reapply "winforms: added customer table""
    This reverts commit 5804881dd145437da18700cdb1dbf3fd1f7880f7.

---
+++ CRM.winforms/Controls/KpiCardControl.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+namespace CRM.winforms.Controls;
+
+public class KpiCardControl : UserControl
+{
+    private readonly Label _lblTitle;
+    private readonly Label _lblValue;
+    private readonly Label _lblBadge;
+    private readonly PictureBox _picIcon;
+
+    private const int CornerRadius = 14;
+    private static readonly Color ColorBorder = Color.FromArgb(232, 226, 226);
+    private static readonly Color ColorTitle = Color.FromArgb(120, 110, 115);
+    private static readonly Color ColorValue = Color.FromArgb(38, 22, 24);
+
+    public KpiCardControl()
+    {
+        this.DoubleBuffered = true;
+        this.SetStyle(ControlStyles.AllPaintingInWmPaint |
+                      ControlStyles.UserPaint |
+                      ControlStyles.OptimizedDoubleBuffer |
+                      ControlStyles.ResizeRedraw, true);
+
+        this.BackColor = Color.Transparent;
+        this.Size = new Size(220, 100);
+        this.Padding = new Padding(14);
+
+        _picIcon = new PictureBox
+        {
+            Location = new Point(14, 14),
+            Size = new Size(18, 18),
+            SizeMode = PictureBoxSizeMode.Zoom,
+            BackColor = Color.Transparent
+        };
+
+        _lblTitle = new Label
+        {
+            Location = new Point(36, 14),
+            AutoSize = true,
+            Font = new Font("Segoe UI Semibold", 9f, FontStyle.Bold),
+            ForeColor = ColorTitle,
+            BackColor = Color.Transparent
+        };
+
+        _lblValue = new Label
+        {
+            Location = new Point(12, 44),
+            AutoSize = true,
+            Font = new Font("Segoe UI", 20f, FontStyle.Bold),
+            ForeColor = ColorValue,
+            BackColor = Color.Transparent
+        };
+
+        _lblBadge = new Label
+        {
+            Location = new Point(80, 54),
+            AutoSize = true,
+            Font = new Font("Segoe UI Semibold", 8f, FontStyle.Bold),
+            Padding = new Padding(5, 2, 5, 2)
+        };
+
+        this.Controls.AddRange(new Control[] { _picIcon, _lblTitle, _lblValue, _lblBadge });
+    }
+
+    public void SetData(string title, string value, string trendText, bool isPositive, Image? icon = null)
+    {
+        _lblTitle.Text = title;
+        _lblValue.Text = value;
+        _picIcon.Image = icon;
+
+        _lblBadge.Text = trendText;
+        _lblBadge.Location = new Point(_lblValue.Right + 8, 54);
+
+        if (isPositive)
+        {
+            _lblBadge.BackColor = Color.FromArgb(232, 247, 238);
+            _lblBadge.ForeColor = Color.FromArgb(35, 120, 68);
+        }
+        else
+        {
+            _lblBadge.BackColor = Color.FromArgb(253, 236, 238);
+            _lblBadge.ForeColor = Color.FromArgb(185, 45, 55);
+        }
+    }
+
+    protected override void OnResize(EventArgs e)
+    {
+        base.OnResize(e);
+
+        if (this.Width > 0 && this.Height > 0)
+        {
+            // Clip OS region to eliminate rectangular corner artifacts
+            using var path = CreateRoundedRect(new Rectangle(0, 0, this.Width, this.Height), CornerRadius);
+            this.Region = new Region(path);
+        }
+    }
+
+    protected override void OnPaint(PaintEventArgs e)
+    {
+        e.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+        e.Graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+        var rect = new Rectangle(0, 0, this.Width - 1, this.Height - 1);
+
+        using var path = CreateRoundedRect(rect, CornerRadius);
+        using var fillBrush = new SolidBrush(Color.White);
+        using var borderPen = new Pen(ColorBorder, 1.2f);
+
+        // Draw solid card face and smooth outline
+        e.Graphics.FillPath(fillBrush, path);
+        e.Graphics.DrawPath(borderPen, path);
+    }
+
+    private static GraphicsPath CreateRoundedRect(Rectangle r, int radius)
+    {
+        var path = new GraphicsPath();
+        int d = radius * 2;
+        path.AddArc(r.X, r.Y, d, d, 180, 90);
+        path.AddArc(r.Right - d, r.Y, d, d, 270, 90);
+        path.AddArc(r.Right - d, r.Bottom - d, d, d, 0, 90);
+        path.AddArc(r.X, r.Bottom - d, d, d, 90, 90);
+        path.CloseFigure();
+        return path;
+    }
+}
--- CRM.winforms/Forms/MainForm.Designer.cs
+++ CRM.winforms/Forms/MainForm.Designer.cs
@@ -28,65 +28,25 @@
         /// </summary>
         private void InitializeComponent()
         {
-            panelSidebar = new Panel();
-            button2 = new Button();
-            buttonCustomers = new Button();
-            buttonDashboard = new Button();
             panelHeader = new Panel();
             logo = new PictureBox();
-            panelSidebar.SuspendLayout();
+            flowLayoutPanelSidebar = new FlowLayoutPanel();
+            buttonDashboard = new Button();
+            buttonCustomers = new Button();
+            button2 = new Button();
+            panelContents = new Panel();
             panelHeader.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)logo).BeginInit();
+            flowLayoutPanelSidebar.SuspendLayout();
             SuspendLayout();
-            // 
-            // panelSidebar
-            // 
-            panelSidebar.BackColor = Color.White;
-            panelSidebar.Controls.Add(button2);
-            panelSidebar.Controls.Add(buttonCustomers);
-            panelSidebar.Controls.Add(buttonDashboard);
-            panelSidebar.Location = new Point(2, -2);
-            panelSidebar.Name = "panelSidebar";
-            panelSidebar.Size = new Size(201, 1080);
-            panelSidebar.TabIndex = 0;
-            panelSidebar.Paint += panel1_Paint;
-            // 
-            // button2
-            // 
-            button2.Location = new Point(10, 208);
-            button2.Name = "button2";
-            button2.Size = new Size(180, 39);
-            button2.TabIndex = 4;
-            button2.Text = "Dashboard";
-            button2.UseVisualStyleBackColor = true;
-            button2.Click += button2_Click;
-            // 
-            // buttonCustomers
-            // 
-            buttonCustomers.Location = new Point(10, 163);
-            buttonCustomers.Name = "buttonCustomers";
-            buttonCustomers.Size = new Size(180, 39);
-            buttonCustomers.TabIndex = 3;
-            buttonCustomers.Text = "Customers";
-            buttonCustomers.UseVisualStyleBackColor = true;
-            buttonCustomers.Click += button1_Click;
-            // 
-            // buttonDashboard
-            // 
-            buttonDashboard.Location = new Point(10, 118);
-            buttonDashboard.Name = "buttonDashboard";
-            buttonDashboard.Size = new Size(180, 39);
-            buttonDashboard.TabIndex = 2;
-            buttonDashboard.Text = "Dashboard";
-            buttonDashboard.UseVisualStyleBackColor = true;
             // 
             // panelHeader
             // 
             panelHeader.BackColor = Color.White;
             panelHeader.Controls.Add(logo);
-            panelHeader.Location = new Point(2, -20);
+            panelHeader.Location = new Point(-3, -20);
             panelHeader.Name = "panelHeader";
-            panelHeader.Size = new Size(1920, 110);
+            panelHeader.Size = new Size(1925, 110);
             panelHeader.TabIndex = 1;
             // 
             // logo
@@ -102,32 +62,84 @@
             logo.TabStop = false;
             logo.Click += logo_Click;
             // 
+            // flowLayoutPanelSidebar
+            // 
+            flowLayoutPanelSidebar.BackColor = Color.White;
+            flowLayoutPanelSidebar.Controls.Add(buttonDashboard);
+            flowLayoutPanelSidebar.Controls.Add(buttonCustomers);
+            flowLayoutPanelSidebar.Controls.Add(button2);
+            flowLayoutPanelSidebar.Location = new Point(-3, 87);
+            flowLayoutPanelSidebar.Name = "flowLayoutPanelSidebar";
+            flowLayoutPanelSidebar.Padding = new Padding(8, 20, 0, 0);
+            flowLayoutPanelSidebar.Size = new Size(201, 955);
+            flowLayoutPanelSidebar.TabIndex = 2;
+            // 
+            // buttonDashboard
+            // 
+            buttonDashboard.Location = new Point(11, 23);
+            buttonDashboard.Name = "buttonDashboard";
+            buttonDashboard.Size = new Size(180, 39);
+            buttonDashboard.TabIndex = 2;
+            buttonDashboard.Text = "Dashboard";
+            buttonDashboard.UseVisualStyleBackColor = true;
+            buttonDashboard.Click += buttonDashboard_Click;
+            // 
+            // buttonCustomers
+            // 
+            buttonCustomers.Location = new Point(11, 68);
+            buttonCustomers.Name = "buttonCustomers";
+            buttonCustomers.Size = new Size(180, 39);
+            buttonCustomers.TabIndex = 3;
+            buttonCustomers.Text = "Customers";
+            buttonCustomers.UseVisualStyleBackColor = true;
+            buttonCustomers.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(11, 113);
+            button2.Name = "button2";
+            button2.Size = new Size(180, 39);
+            button2.TabIndex = 4;
+            button2.Text = "Dashboard";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // panelContents
+            // 
+            panelContents.BackColor = Color.Transparent;
+            panelContents.Location = new Point(194, 87);
+            panelContents.Name = "panelContents";
+            panelContents.Size = new Size(1712, 955);
+            panelContents.TabIndex = 3;
+            panelContents.Paint += panelContents_Paint;
+            // 
             // MainForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(224, 224, 224);
             ClientSize = new Size(1904, 1041);
+            Controls.Add(panelContents);
+            Controls.Add(flowLayoutPanelSidebar);
             Controls.Add(panelHeader);
-            Controls.Add(panelSidebar);
             MinimumSize = new Size(1100, 700);
             Name = "MainForm";
             Text = "Vantage CRM";
             WindowState = FormWindowState.Maximized;
             Load += MainForm_Load;
-            panelSidebar.ResumeLayout(false);
             panelHeader.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)logo).EndInit();
+            flowLayoutPanelSidebar.ResumeLayout(false);
             ResumeLayout(false);
         }
 
         #endregion
-
-        private Panel panelSidebar;
         private Panel panelHeader;
         private PictureBox logo;
-        private Button buttonCustomers;
+        private FlowLayoutPanel flowLayoutPanelSidebar;
         private Button buttonDashboard;
+        private Button buttonCustomers;
         private Button button2;
+        private Panel panelContents;
     }
 }
