f587517d9986e4e8b58662f591b695fa5b9f1e37
JoshuaAmbos <j.ambos.554298@umindanao.edu.ph>
2026-09-24 15:59:27 +0800

    refactor LoyaltyAwardsView: remove unused using directives for improved clarity

--- CRM.winforms/Views/LoyaltyAwardsViews/LoyaltyAwardsView.cs
+++ CRM.winforms/Views/LoyaltyAwardsViews/LoyaltyAwardsView.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace CRM.winforms.Views.LoyaltyAwardsViews
+﻿namespace CRM.winforms.Views.LoyaltyAwardsViews
 {
     internal class LoyaltyAwardsView
     {
